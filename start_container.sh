93ef1ded8125f8fd244467580a72284dbdb66945
Sahilfs17 <87992279+Sahilfs17@users.noreply.github.com>
2025-02-04 09:56:34 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+# Stop the running container (if any)
+docker pull sahilfs17/simple-python-flask-app
 
-# Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sahilfs17/simple-python-flask-app
