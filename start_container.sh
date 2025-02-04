0535709639e106f51843c200f47556f1d586adc0
Sahilfs17 <87992279+Sahilfs17@users.noreply.github.com>
2025-02-04 10:27:48 +0530

    m

--- start_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Stop the running container (if any)
-docker pull sahilfs17/simple-python-flask-app
-
-docker run -d -p 5000:5000 sahilfs17/simple-python-flask-app
--- stop_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-containerid = `docker ps | awk -F " " '{print $1}'`
-
-docker rm -f $containerid
